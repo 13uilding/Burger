3e977b393439909713d8aeb0ae0e9b7b152562d2
deep <hereisdanbrown@gmail.com>
2020-02-01 20:48:21 -0700

    still heroku

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -7,12 +7,14 @@ var burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
-  burger.all(function(data) {
-    var hbsObject = {
-      burgers: data
-    };
-    res.render("index", hbsObject);
-  });
+  res.send("Hello, World");
+
+  // burger.all(function(data) {
+  //   var hbsObject = {
+  //     burgers: data
+  //   };
+  //   res.render("index", hbsObject);
+  // });
 });
 
 router.post("/api/burgers", function(req, res) {
